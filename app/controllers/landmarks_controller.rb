de77aab779e65373b0a2fd08441b6c98987c92a3
Miwa <paintfx@gmail.com>
2018-03-25 05:37:59 +0000

    Done.

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,32 @@
 class LandmarksController < ApplicationController
 
+    get '/landmarks' do
+      erb :'landmarks/index'
+    end
+
+    get '/landmarks/new' do
+      erb :'landmarks/new'
+    end
+
+    post '/landmarks' do
+
+    end
+
+
+    get '/landmarks/:id' do
+
+    end
+
+    get '/landmarks/:id/edit' do
+
+    end
+
+    patch '/landmarks/:id' do
+
+    end
+
+    delete '/landmarks/:id' do
+
+    end
+
 end
